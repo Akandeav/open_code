ec8a95dff5afbfce8582b5573eae976aa7804eac
akandeav <akandevic@gmail.com>
2023-01-31 01:42:33 +0100

    feat: number of active github repo tabs

--- popups/popup.js
+++ popups/popup.js
@@ -1,20 +1,32 @@
-async function getCurrentTab(){
-    // getCurrentTab()
-    //
-    // gets the currently-focused window or most recently-focused window
-    // if no chrome windows are focused. it returns a tab instance or undefined
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions)
-    return tab;
-}
 // gets opened github tabs
+// the url is to match any repository, however
 const github_tabs = await chrome.tabs.query({
     url: [
-        "https://*.github.com/*"
+        "https://github.com/*/*"
     ]
 })
-const current_tab = getCurrentTab()
-const current_url = current_tab.url
+let numOfActiveTabs = 0;
 
-document.getElementById("numOpenGithubTabs").innerHTML = github_tabs.length()
 
+const template = document.getElementById("li_template");
+const elements = new Set();
+for (const tab of github_tabs){
+    const element = template.content.firstElementChild.cloneNode(true);
+
+    const title = tab.title.split("-")[0].trim();
+    const pathname = new URL(tab.url).pathname;
+
+    element.querySelector(".title").textContent = title
+    element.querySelector(".pathname").textContent = pathname
+    element.querySelector("a").addEventListener("click", async () => {
+        // open the github repository path in vscode web
+        chrome.tabs.create({
+            url: "http://vscode.dev/github" + pathname
+        });
+    });
+
+    elements.add(element)
+    numOfActiveTabs += 1;
+}
+document.querySelector("ul").append(...elements);
+document.getElementById("numOfOpenGithubTabs").innerHTML = numOfActiveTabs;
