95e985aa10303ba60b06f91ea855902a94703be2
akandeav <akandevic@gmail.com>
2023-01-31 14:50:30 +0100

    refactor: display message for no active tabs

--- popups/popup.js
+++ popups/popup.js
@@ -1,32 +1,40 @@
+let numOfActiveGithubTabs = 0;
+// what displays in the paragraph tag that shows how many github
+// tabs are active
+let tabStatusElementInner = ""; 
+
 // gets opened github tabs
-// the url is to match any repository, however
-const github_tabs = await chrome.tabs.query({
+// the url is to match any repository
+const githubTabs = await chrome.tabs.query({
     url: [
         "https://github.com/*/*"
     ]
 })
-let numOfActiveTabs = 0;
-
 
 const template = document.getElementById("li_template");
 const elements = new Set();
-for (const tab of github_tabs){
+for (const tab of githubTabs){
     const element = template.content.firstElementChild.cloneNode(true);
 
-    const title = tab.title.split("-")[0].trim();
+    const title = tab.title.split("-")[0].trim().split("/")[1];
     const pathname = new URL(tab.url).pathname;
-
     element.querySelector(".title").textContent = title
     element.querySelector(".pathname").textContent = pathname
-    element.querySelector("a").addEventListener("click", async () => {
-        // open the github repository path in vscode web
-        chrome.tabs.create({
-            url: "http://vscode.dev/github" + pathname
-        });
+    element.querySelector("a").addEventListener(
+        "click", async () => {
+            // open the github repository path in vscode web
+            chrome.tabs.create({
+                url: "https://vscode.dev/github" + pathname
+            });
     });
 
     elements.add(element)
-    numOfActiveTabs += 1;
+    numOfActiveGithubTabs += 1;
 }
 document.querySelector("ul").append(...elements);
-document.getElementById("numOfOpenGithubTabs").innerHTML = numOfActiveTabs;
+
+numOfActiveGithubTabs > 0 ?
+    tabStatusElementInner = numOfActiveGithubTabs + " active github repo(s)"
+    : tabStatusElementInner = "No active github repository";
+
+document.getElementById( "numOfOpenGithubTabs").innerHTML = tabStatusElementInner
